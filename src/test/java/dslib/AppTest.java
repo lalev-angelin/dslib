2e57c9b4aaeea32579b07833e1e49af3bcd5c8b5
Angelin Lalev <lalev@uni-svishtov.bg>
2022-02-24 12:31:15 +0200

    Separating parsers in their own classes

--- src/test/java/dslib/AppTest.java
+++ src/test/java/dslib/AppTest.java
@@ -2,11 +2,7 @@ package dslib;
 
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for simple App.
@@ -108,8 +104,8 @@ public class AppTest
 
     @Test
     public void testDSPairCompare() {
-        var a = new DSPair<>(1,2);
-        var b = new DSPair<>(1,2);
+        DSPair<Integer, Integer> a = new DSPair<>(1,2);
+        DSPair<Integer, Integer> b = new DSPair<>(1,2);
         assertEquals(0, a.compareTo(b));
 
         a = new DSPair<>(2,2);
@@ -124,120 +120,142 @@ public class AppTest
     }
 
     @Test
-    public void testDSSetIsSetRepresentation() {
-        assertFalse(DSSet.isSetRepresentation("{"));
-        assertTrue(DSSet.isSetRepresentation("{}"));
-        assertFalse(DSSet.isSetRepresentation("}"));
-    }
+    public void testDSSParserDetect() {
+        assertSame(DSSetParser.detect("{}"), DSSetParser.ElementType.ET_EMPTY_DSSET);
+        assertSame(DSSetParser.detect("{  }"), DSSetParser.ElementType.ET_EMPTY_DSSET);
+        assertSame(DSSetParser.detect("{a,b,c}"), DSSetParser.ElementType.ET_DSSET);
+        assertSame(DSSetParser.detect("{{a,b,c}}"), DSSetParser.ElementType.ET_DSSET);
+        assertSame(DSSetParser.detect("{a,b,{c}}"), DSSetParser.ElementType.ET_DSSET);
+        assertSame(DSSetParser.detect("{(a,b),{c}}"), DSSetParser.ElementType.ET_DSSET);
+        assertSame(DSSetParser.detect("{(c)}"), DSSetParser.ElementType.ET_DSSET);
+        assertSame(DSSetParser.detect("{(),{c}}"), DSSetParser.ElementType.ET_DSSET);
 
-    @Test
-    public void testDSPairIsPairRepresentation() {
-        assertTrue(DSPair.isPairRepresentation("(1,2)"));
-        assertFalse(DSPair.isPairRepresentation("(1,2,3)"));
-        assertTrue(DSPair.isPairRepresentation("({1,2},{})"));
-        assertFalse(DSPair.isPairRepresentation("({1,2}, {1"));
-        assertFalse(DSPair.isPairRepresentation("({1,2}, {1}"));
-        assertFalse(DSPair.isPairRepresentation("({1,2}, {1)"));
-        // Should we have tuples with empty elements ?
-        assertTrue(DSPair.isPairRepresentation("(,)"));
-        assertTrue(DSPair.isPairRepresentation("(1,)"));
-        assertTrue(DSPair.isPairRepresentation("(,1)"));
-    }
-
-    @Test
-    public void testDSPairInverse() {
-        var a = new DSPair<>(1,2);
-        var b = new DSPair<>(2,1);
-        assertTrue(a.equalsTo(b.invert()));
-    }
-
-    @Test
-    public void testDSPairParse() {
-        DSPair<Integer, String> a;
-        DSPair<Integer, String> b = new DSPair<>(1, "abds");
-        assertDoesNotThrow(()->{
-            DSPair<Integer, String> c = DSPair.parse("(1, abds)", Integer::parseInt, (s)->s);
-        });
-
-        a = DSPair.parse("(1, abds)", Integer::parseInt, (s)->s);
-        assertTrue(a.equalsTo(b));
-    }
-
-    @Test
-    public void testDSSetParse() {
-        DSSet s = DSSet.parse("{a,b,c}");
-        DSSet stest = new DSSet(new DSValue<>("a"), new DSValue<>("b"), new DSValue<>("c"));
-        assertTrue(s.equalsTo(stest));
-
-        s = DSSet.parse("{a, b, c}");
-        assertTrue(s.equalsTo(stest));
-
-        s = DSSet.parse("{ a,  b , c }");
-        assertTrue(s.equalsTo(stest));
-
-        stest = new DSSet(new DSSet(), new DSSet(new DSValue<>("a")), new DSValue<>("a"));
-        s = DSSet.parse("{ {}, { a }, a   } ");
-        assertTrue(s.equalsTo(stest));
-
-        stest = new DSSet(new DSPair<>("1","2"), new DSValue<>("1"), new DSValue<>("a"));
-        s = DSSet.parse("{(1,2),1,a}");
-        assertTrue(s.equalsTo(stest));
-    }
-
-    @Test
-    public void testDSSetIntersect() {
-        var q = DSSet.parse("{a,b,c}");
-        var p = DSSet.parse("{b,c}");
-        assertTrue(q.intersect(p).equalsTo(p));
-
-        q = DSSet.parse("{{}, a, b}");
-        p = DSSet.parse("{}");
-        assertTrue(q.intersect(p).equalsTo(p));
-        assertTrue(p.intersect(q).equalsTo(p));
-
-        p = DSSet.parse("{{}}");
-        assertTrue(p.intersect(q).equalsTo(p));
-        assertTrue(q.intersect(p).equalsTo(p));
-
-        p = DSSet.parse("{{ }}");
-        assertTrue(p.intersect(q).equalsTo(p));
-        assertTrue(q.intersect(p).equalsTo(p));
-    }
-
-    @Test
-    public void testDSSJoin() {
-        var p = DSSet.parse("{a,b,c}");
-        var q = DSSet.parse("{d, e, f, a}");
-        var r = DSSet.parse("{a,b,c,d,e,f}");
-        assertTrue(p.join(q).equalsTo(r));
-
-        p = DSSet.parse("{}");
-        assertTrue(p.join(p).equalsTo(p));
-
-        p = DSSet.parse("{a,a,a}");
-        q = DSSet.parse("{a}");
-        assertTrue(p.equalsTo(q));
-
-        q = DSSet.parse("{a,b}");
-        assertTrue(p.equalsTo(q));
-
-        p = DSSet.parse("{a, (1,2), {1,2}}");
-        q = DSSet.parse("{4,5}");
-        r = DSSet.parse("{{4,5}, a, (1,2), {1,2}}");
+        assertSame(DSSetParser.detect("(1,2)"), DSSetParser.ElementType.ET_DSPAIR);
+        assertSame(DSSetParser.detect("((1,2),2)"), DSSetParser.ElementType.ET_DSPAIR);
+        assertSame(DSSetParser.detect("(\"1\",2)"), DSSetParser.ElementType.ET_DSPAIR);
+        assertSame(DSSetParser.detect("({1,2},2)"), DSSetParser.ElementType.ET_DSPAIR);
+        assertSame(DSSetParser.detect("({1,2},{3,4})"), DSSetParser.ElementType.ET_DSPAIR);
+        assertSame(DSSetParser.detect("(\"1\",\"2,,,,\", 4)"), DSSetParser.ElementType.ET_DSPAIR);
 
     }
 
-    @Test
-    public void testDSSSubtract() {
-        DSSet p = DSSet.parse("{a,b,c}");
-        DSSet q = DSSet.parse("{a,b}");
-        assertTrue(p.subtract(q).equalsTo(DSSet.parse("{c}")));
-        assertTrue(q.subtract(p).equalsTo(DSSet.parse("{}")));
 
-        p = DSSet.parse("{(1,2),{(1,2),a,b}");
-        q = DSSet.parse("{{b,a,(1,2)}, (1,2)}");
-        assertTrue(p.subtract(q).equalsTo(new DSSet()));
-    }
+//
+//    @Test
+//    public void testDSSetIsSetRepresentation() {
+//        assertFalse(DSSet.isSetRepresentation("{"));
+//        assertTrue(DSSet.isSetRepresentation("{}"));
+//        assertFalse(DSSet.isSetRepresentation("}"));
+//    }
+//
+//    @Test
+//    public void testDSPairIsPairRepresentation() {
+//        assertTrue(DSPair.isPairRepresentation("(1,2)"));
+//        assertFalse(DSPair.isPairRepresentation("(1,2,3)"));
+//        assertTrue(DSPair.isPairRepresentation("({1,2},{})"));
+//        assertFalse(DSPair.isPairRepresentation("({1,2}, {1"));
+//        assertFalse(DSPair.isPairRepresentation("({1,2}, {1}"));
+//        assertFalse(DSPair.isPairRepresentation("({1,2}, {1)"));
+//        // Should we have tuples with empty elements ?
+//        assertTrue(DSPair.isPairRepresentation("(,)"));
+//        assertTrue(DSPair.isPairRepresentation("(1,)"));
+//        assertTrue(DSPair.isPairRepresentation("(,1)"));
+//    }
+//
+//    @Test
+//    public void testDSPairInverse() {
+//        DSPair<Integer, Integer> a = new DSPair<>(1,2);
+//        DSPair<Integer, Integer> b = new DSPair<>(2,1);
+//        assertTrue(a.equalsTo(b.invert()));
+//    }
+//
+//    @Test
+//    public void testDSPairParse() {
+//        DSPair<Integer, String> a;
+//        DSPair<Integer, String> b = new DSPair<>(1, "abds");
+//        assertDoesNotThrow(()->{
+//            DSPair<Integer, String> c = DSPair.parse("(1, abds)", Integer::parseInt, (s)->s);
+//        });
+//
+//        a = DSPair.parse("(1, abds)", Integer::parseInt, (s)->s);
+//        assertTrue(a.equalsTo(b));
+//    }
+//
+//    @Test
+//    public void testDSSetParse() {
+//        DSSet s = DSSet.parse("{a,b,c}");
+//        DSSet stest = new DSSet(new DSValue<>("a"), new DSValue<>("b"), new DSValue<>("c"));
+//        assertTrue(s.equalsTo(stest));
+//
+//        s = DSSet.parse("{a, b, c}");
+//        assertTrue(s.equalsTo(stest));
+//
+//        s = DSSet.parse("{ a,  b , c }");
+//        assertTrue(s.equalsTo(stest));
+//
+//        stest = new DSSet(new DSSet(), new DSSet(new DSValue<>("a")), new DSValue<>("a"));
+//        s = DSSet.parse("{ {}, { a }, a   } ");
+//        assertTrue(s.equalsTo(stest));
+//
+//        stest = new DSSet(new DSPair<>("1","2"), new DSValue<>("1"), new DSValue<>("a"));
+//        s = DSSet.parse("{(1,2),1,a}");
+//        assertTrue(s.equalsTo(stest));
+//    }
+//
+//    @Test
+//    public void testDSSetIntersect() {
+//        DSSet q = DSSet.parse("{a,b,c}");
+//        DSSet p = DSSet.parse("{b,c}");
+//        assertTrue(q.intersect(p).equalsTo(p));
+//
+//        q = DSSet.parse("{{}, a, b}");
+//        p = DSSet.parse("{}");
+//        assertTrue(q.intersect(p).equalsTo(p));
+//        assertTrue(p.intersect(q).equalsTo(p));
+//
+//        p = DSSet.parse("{{}}");
+//        assertTrue(p.intersect(q).equalsTo(p));
+//        assertTrue(q.intersect(p).equalsTo(p));
+//
+//        p = DSSet.parse("{{ }}");
+//        assertTrue(p.intersect(q).equalsTo(p));
+//        assertTrue(q.intersect(p).equalsTo(p));
+//    }
+//
+//    @Test
+//    public void testDSSJoin() {
+//        DSSet p = DSSet.parse("{a,b,c}");
+//        DSSet q = DSSet.parse("{d, e, f, a}");
+//        DSSet r = DSSet.parse("{a,b,c,d,e,f}");
+//        assertTrue(p.join(q).equalsTo(r));
+//
+//        p = DSSet.parse("{}");
+//        assertTrue(p.join(p).equalsTo(p));
+//
+//        p = DSSet.parse("{a,a,a}");
+//        q = DSSet.parse("{a}");
+//        assertTrue(p.equalsTo(q));
+//
+//        q = DSSet.parse("{a,b}");
+//        assertTrue(p.equalsTo(q));
+//
+//        p = DSSet.parse("{a, (1,2), {1,2}}");
+//        q = DSSet.parse("{4,5}");
+//        r = DSSet.parse("{{4,5}, a, (1,2), {1,2}}");
+//
+//    }
+//
+//    @Test
+//    public void testDSSSubtract() {
+//        DSSet p = DSSet.parse("{a,b,c}");
+//        DSSet q = DSSet.parse("{a,b}");
+//        assertTrue(p.subtract(q).equalsTo(DSSet.parse("{c}")));
+//        assertTrue(q.subtract(p).equalsTo(DSSet.parse("{}")));
+//
+//        p = DSSet.parse("{(1,2),{(1,2),a,b}");
+//        q = DSSet.parse("{{b,a,(1,2)}, (1,2)}");
+//        assertTrue(p.subtract(q).equalsTo(new DSSet()));
+//    }
 
     {
         assertTrue( true );
